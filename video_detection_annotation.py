46ccbbf1cda01413b2269d58ee13e211d1d70144
Min Jae <reccos7@naver.com>
2021-09-25 16:54:13 +0900

    Object Detection API + Annotation

---
+++ video_detection_annotation.py
@@ -0,0 +1,5 @@
+from objection_detect_annotate import DetectionObj
+
+if __name__ == "__main__":
+    detection=DetectionObj(model='ssd_mobilenet_v1_coco_11_06_2017')
+    detection.video_pipeline(video=".sample_videos/ducks.mp4, audio=False")
